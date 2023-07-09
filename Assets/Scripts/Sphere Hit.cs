1bba4b9bfae9f9629e5d9321b65c1191e1288abc
dalexandre_12@outlook.com <dalexandre_12@outlook.com>
2023-07-09 05:01:56 -0400

    ai done, borders done, + ui improvements

--- Assets/Scripts/Sphere Hit.cs
+++ Assets/Scripts/Sphere Hit.cs
@@ -11,19 +11,35 @@ public class SphereHit : MonoBehaviour
     {
         if (collision.gameObject.tag == "Cue Stick")
         {
-            Debug.Log("HIT");
-            gameObject.GetComponent<Rigidbody>().AddForce((gameObject.transform.position 
-                - collision.gameObject.GetComponent<CueHit>().tip.transform.position).normalized * hitPower);
+            gameObject.GetComponent<Rigidbody>().AddForce((collision.gameObject.GetComponent<CueHit>().dir).normalized * hitPower);
             collision.gameObject.GetComponentInParent<CueHit>().isFadingOut = true;
             wasHit = true;
         }
+        else if (collision.gameObject.tag == "Ball")
+        {
+            Debug.DrawRay(collision.gameObject.transform.position, (gameObject.transform.position - collision.gameObject.transform.position).normalized, Color.green, 10);
+            var dir = (gameObject.transform.position - collision.gameObject.transform.position).normalized;
+            gameObject.GetComponent<Rigidbody>().AddForce(dir * (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude) * 25f);
+            collision.gameObject.GetComponent<Rigidbody>().velocity *= 0.5f;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered Pocket");
     }
 
     void FixedUpdate()
     {
-        Debug.Log(gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude);
-        if (gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude < 1.2f ) {
+        //Debug.Log(gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude);
+        if (gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude < 2f) {
+            gameObject.GetComponent<Rigidbody>().velocity *= 0.95f * Time.deltaTime;
+
+        }
+        else if (gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude < 1f)
+        {
             gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            hitPower = 1100f;
         }
     }
 }
